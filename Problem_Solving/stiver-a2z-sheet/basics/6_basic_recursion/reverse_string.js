9cd0e4fb276b5210f1b64ab67ea81f41a3bbba64
Prathamesh Mali <prathamesh@rideblitz.com>
2025-08-26 12:55:05 +0530

    added optimized solution

--- Problem_Solving/stiver-a2z-sheet/basics/6_basic_recursion/reverse_string.js
+++ Problem_Solving/stiver-a2z-sheet/basics/6_basic_recursion/reverse_string.js
@@ -14,5 +14,28 @@ class Solution {
     // return the new array with last and sliced array
     return [last, ...this.reverseString(slicedArr)]
   }
+
+
+  // optimized
+  reverStringOptimized(s) {
+
+    // helper function for swapping
+    const swap = (left, right, s) => {
+      if (left > right) return
+      [s[left], s[right]] = [s[right], s[left]]
+
+      return swap(left + 1, right - 1, s)
+    }
+
+    // call the helper function
+    swap(0, s.length - 1, s)
+
+    // return reverse string
+    return s
+  }
+
+
 }
 
+const soln = new Solution()
+console.log(soln.reverStringOptimized(["h", "e", "l", "l", "o"]))
--- Problem_Solving/stiver-a2z-sheet/basics/6_basic_recursion/sum_of_arr.js
+++ Problem_Solving/stiver-a2z-sheet/basics/6_basic_recursion/sum_of_arr.js
@@ -1,11 +1,38 @@
 class Solution {
+  // arraySum(nums) {
+  //   // base case
+  //   if (nums.length === 0) {
+  //     return 0
+  //   }
+  //
+  //   // add removed n from the start and 
+  //   return nums.shift() + this.arraySum(nums)
+  // }
+
+
+  // with helper function
+  sum(i, nums) {
+    if (i >= nums.length) {
+      return 0;
+    }
+    return nums[i] + this.sum(i + 1, nums);
+  }
   arraySum(nums) {
-    // base case
-    if (nums.length === 0) {
+    return this.sum(0, nums);
+  }
+
+
+
+  // parameterized soln
+  arrSumParamterized(i, nums) {
+    if (i >= nums.length) {
       return 0
     }
-
-    // add removed n from the start and 
-    return nums.shift() + this.arraySum(nums)
+    return nums[i] + this.arrSumParamterized(i + 1, nums)
   }
-} 
+}
+
+const soln = new Solution()
+
+console.log(soln.arrSumParamterized(0, [1, 2, 3, 4]))
+console.log(soln.arraySum([1, 2, 3, 4]))
