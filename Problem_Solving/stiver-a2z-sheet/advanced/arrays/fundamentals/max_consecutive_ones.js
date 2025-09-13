61a379decd92e2f4ab7a6fb7f08b71300c5d23ed
Prathamesh Mali <prathamesh@rideblitz.com>
2025-09-13 12:28:24 +0530

    added optimized solution

--- Problem_Solving/stiver-a2z-sheet/advanced/arrays/fundamentals/max_consecutive_ones.js
+++ Problem_Solving/stiver-a2z-sheet/advanced/arrays/fundamentals/max_consecutive_ones.js
@@ -1,25 +1,51 @@
 class Solution {
+  // findMaxConsecutiveOnes(nums) {
+  //   let maxCount = 0
+  //   let count = 0
+  //
+  //   for (let i = 0; i < nums.length; i++) {
+  //     // increase the count when found 1
+  //     if (nums[i] === 1) {
+  //       count++
+  //     }
+  //
+  //     // break the count if found 0
+  //     if (nums[i] === 0) {
+  //       count = 0
+  //     }
+  //
+  //     // check if the current count is greater than max count
+  //     if (count > maxCount) {
+  //       maxCount = count
+  //     }
+  //   }
+  //
+  //   return maxCount
+  // }
+
+  // optimized
   findMaxConsecutiveOnes(nums) {
     let maxCount = 0
     let count = 0
 
     for (let i = 0; i < nums.length; i++) {
-      // increase the count when found 1
       if (nums[i] === 1) {
+        // increment the count if 1 is found
         count++
-      }
 
-      // break the count if found 0
-      if (nums[i] === 0) {
+        // set the max count between maxcount and count
+        if (count > maxCount) {
+          maxCount = count
+        }
+        // maxCount = Math.max(maxCount, count)
+
+      }
+      else {
+        // reset the count of count when 0 is found
         count = 0
       }
 
-      // check if the current count is greater than max count
-      if (count > maxCount) {
-        maxCount = count
-      }
     }
-
     return maxCount
   }
 }
