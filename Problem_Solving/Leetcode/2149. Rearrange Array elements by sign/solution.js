f19c5fcb72df2350ce41bb10d76af6de45e3b426
Prathamesh Mali <prathamesh@rideblitz.com>
2025-09-21 15:36:23 +0530

    update variables naming

--- Problem_Solving/Leetcode/2149. Rearrange Array elements by sign/solution.js
+++ Problem_Solving/Leetcode/2149. Rearrange Array elements by sign/solution.js
@@ -6,16 +6,16 @@ var rearrangeArray = function(nums) {
   let N = nums.length
   let res = new Array(N).fill(0)
 
-  let i = 0
-  let j = 1
+  let pos = 0
+  let neg = 1
 
   for (let n of nums) {
     if (n > 0) {
-      res[i] = n
-      i += 2
+      res[pos] = n
+      pos += 2
     } else {
-      res[j] = n
-      j += 2
+      res[neg] = n
+      neg += 2
     }
   }
 
