67afe20835331bc57d674453b35be9a6e2c9c4eb
Prathamesh Mali <prathamesh@rideblitz.com>
2025-09-29 20:56:02 +0530

    added updateed oln

--- Problem_Solving/stiver-a2z-sheet/advanced/arrays/FAQ (Medium)/pascals_triangle_1.js
+++ Problem_Solving/stiver-a2z-sheet/advanced/arrays/FAQ (Medium)/pascals_triangle_1.js
@@ -2,12 +2,14 @@ class Solution {
   pascalTriangleI(r, c) {
     let res = 1
 
+    /*
+     * ( (n/k)
+     * */
     // for one based index (minus from the row and columns)
     let rows = r - 1
     let columns = c - 1
     for (let i = 0; i < columns; i++) {
       // formula 
-      // res = res * (rows -1) / (i+1)
       res = res * (rows - i) / (i + 1)
     }
 
