d15b0891041b6bafb02791c742702ced245414af
Prathamesh Mali <prathamesh@rideblitz.com>
2025-10-09 23:07:20 +0530

    addes solution for lc 704 binary search

---
+++ Problem_Solving/Leetcode/704. Binary Search/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let op = -1
+
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      left++
+    } else {
+      right--
+    }
+  }
+
+  return -1
+
+}
