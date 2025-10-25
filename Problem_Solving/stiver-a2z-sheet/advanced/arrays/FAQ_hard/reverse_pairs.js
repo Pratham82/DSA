6ba6802d0799999fec21d79e6e04a602a60e1a8a
Prathamesh Mali <prathamesh@rideblitz.com>
2025-10-25 14:52:48 +0530

    added brute force solution

---
+++ Problem_Solving/stiver-a2z-sheet/advanced/arrays/FAQ_hard/reverse_pairs.js
@@ -0,0 +1,29 @@
+class Solution {
+  // reversePairs(nums) {
+  //   let N = nums.length
+  //   let count = 0
+  //
+  //   for (let i = 0; i < N; i++) {
+  //     for (let j = i + 1; j < N; j++) {
+  //       if (nums[i] > nums[j] * 2) {
+  //         count++
+  //       }
+  //     }
+  //   }
+  //
+  //   return count
+  // }
+
+  reversePairs(nums) {
+
+    // merge sort
+
+
+    // merge function
+
+  }
+}
+
+const soln = new Solution()
+console.log(soln.reversePairs([6, 4, 1, 2, 7]))
+console.log(soln.reversePairs([5, 4, 4, 3, 3]))
