b077675119dd9c81c2259286307a7151848921b3
Prathamesh Mali <prathamesh@rideblitz.com>
2025-10-25 23:12:41 +0530

    added merge sort solution

--- Problem_Solving/stiver-a2z-sheet/advanced/arrays/FAQ_hard/reverse_pairs.js
+++ Problem_Solving/stiver-a2z-sheet/advanced/arrays/FAQ_hard/reverse_pairs.js
@@ -15,12 +15,61 @@ class Solution {
   // }
 
   reversePairs(nums) {
-
     // merge sort
+    function mergeSort(nums, left, right) {
+      if (left >= right) return 0
 
+      const mid = Math.floor((left + right) / 2)
+      let count = 0
+
+      // count left & right halves
+      count += mergeSort(nums, left, mid)
+      count += mergeSort(nums, mid + 1, right)
+
+      // count cross pairs
+      count += countCrossPairs(nums, left, mid, right)
+
+      // merge sorted values
+      merge(nums, left, mid, right)
+
+      return count
+    }
+
+    // count pairs
+    function countCrossPairs(nums, left, mid, right) {
+      let count = 0
+      let j = mid + 1
+      for (let i = left; i <= mid; i++) {
+        while (j <= right && nums[i] > 2 * nums[j]) j++
+        count += (j - (mid + 1))
+      }
+
+      return count
+    }
 
     // merge function
+    function merge(nums, left, mid, right) {
+      const temp = []
+      let i = left, j = mid + 1
 
+
+      while (i <= mid && j <= right) {
+        // nums[i++] is same as below: 
+        // temp.push(nums[i]);
+        // i = i + 1;
+        if (nums[i] <= nums[j]) temp.push(nums[i++])
+        else temp.push(nums[j++])
+      }
+
+      while (i <= mid) temp.push(nums[i++])
+      while (j <= right) temp.push(nums[j++])
+
+      for (let k = left; k <= right; k++) {
+        nums[k] = temp[k - left]
+      }
+    }
+
+    return mergeSort(nums, 0, nums.length - 1)
   }
 }
 
